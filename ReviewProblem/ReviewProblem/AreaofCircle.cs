4083ae828aa516ef765623f4b50cc8550981350b
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-15 16:30:11 +0530

    [gurpreet] AreaofCircle

---
+++ ReviewProblem/ReviewProblem/AreaofCircle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewProgram
+{
+    internal class AreaofCircle
+    {
+        public void areaOfcircle()
+
+        {
+            double radius, area;
+            Console.WriteLine("Enter Radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * radius * radius;
+            Console.WriteLine("\nArea of circle: " + area);
+        }
+    }
+}
+
--- ReviewProblem/ReviewProblem/Program.cs
+++ ReviewProblem/ReviewProblem/Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("press1 to perimeter of rectangle:");
+            Console.WriteLine("press2 to area of circle:");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -12,6 +13,11 @@
                 case 1:
                     PerimeterofRectangle perimeter = new PerimeterofRectangle();
                     perimeter.area();
+                    break;
+
+                case 2:
+                    AreaofCircle areaofcircle = new AreaofCircle();
+                    areaofcircle.areaOfcircle();
                     break;
             }
         }
