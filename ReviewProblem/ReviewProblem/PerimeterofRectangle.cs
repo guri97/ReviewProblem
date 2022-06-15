0e4bb043ef56b61857328636d4ad5d9a4e47d639
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-15 16:26:05 +0530

    [gurpreet] PerimeterofRectangle

---
+++ ReviewProblem/ReviewProblem/PerimeterofRectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewProgram
+{
+    internal class PerimeterofRectangle
+    {
+        public void area()
+        {
+            float l, w, perimeter;
+            Console.Write("Enter the value of length: ");
+            l = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the value of width: ");
+            w = Convert.ToInt32(Console.ReadLine());
+
+            perimeter = 2 * (l + w);
+
+            Console.WriteLine("Perimeter of rectangle: " + perimeter);
+
+        }
+    }
+}
---
+++ ReviewProblem/ReviewProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ReviewProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("press1 to perimeter of rectangle:");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    PerimeterofRectangle perimeter = new PerimeterofRectangle();
+                    perimeter.area();
+                    break;
+            }
+        }
+    }
+}
+
