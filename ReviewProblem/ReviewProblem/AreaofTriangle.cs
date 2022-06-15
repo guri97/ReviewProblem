4d47995ce4a1c1b6e9e0eda8e3c32d1d0617bcfb
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-15 16:33:54 +0530

    [gurpreet] AreaofTriangle

---
+++ ReviewProblem/ReviewProblem/AreaofTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewProgram
+{
+    internal class AreaofTriangle
+    {
+        public void areaoftrianagle()
+        {
+            Console.WriteLine("Enter the base of triangle:");
+            double triangleBase = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the height of triangle:");
+            double triangleHeight = Convert.ToDouble(Console.ReadLine());
+            double areaOfTriangle = (triangleBase * triangleHeight) / 2;
+            Console.WriteLine("Area of triangle is: " + areaOfTriangle);
+        }
+    }
+}
--- ReviewProblem/ReviewProblem/Program.cs
+++ ReviewProblem/ReviewProblem/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("press1 to perimeter of rectangle:");
             Console.WriteLine("press2 to area of circle:");
-
+            Console.WriteLine("press 3 to area of triangle");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -18,6 +18,11 @@
                 case 2:
                     AreaofCircle areaofcircle = new AreaofCircle();
                     areaofcircle.areaOfcircle();
+                    break;
+
+                case 3:
+                    AreaofTriangle areaoftrianagle = new AreaofTriangle();
+                    areaoftrianagle.areaoftrianagle();
                     break;
             }
         }
